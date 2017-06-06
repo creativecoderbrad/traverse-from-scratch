8383274e3d324d9f9cf3aaa2608a19b4cec74695
creativecoderbrad <brad_124@hotmail.co.uk>
2017-06-06 17:22:41 +0100

    latest push

--- server.js
+++ server.js
@@ -3,28 +3,52 @@
 const express = require('express');
 const pug = require('pug');
 const path = require('path');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-
-mongoose.connect('mongodb://localhost/nodekb');
-var db = mongoose.connection;
-
-// check connection
-db.once('open', function(){
-  console.log('Connected to mongoDB ...');
-});
-
-// check for db err
-db.on('error', function(err){
-  console.log(err);
-});
-
 
 // initialise our app
 const app = express();
 
 
+// load our view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+
+app.get('/', function(req, res){
+
+var articles = [
+{
+  id: 1,
+  title: 'Article one',
+  author: 'brad lumber',
+  body: 'this is article one which has text'
+},
+{
+  id: 2,
+  title: 'Article two',
+  author: 'john doe',
+  body: 'this is article two which has text'
+},
+{
+  id: 3,
+  title: 'Article three',
+  author: 'jane doe',
+  body: 'this is article three which has text'
+}
+];
+
+
+  res.render('index', {
+    title: 'articles',
+    articles: articles
+  });
+});
+
+app.get('/articles/add', function(req, res){
+  res.render('add_article', {
+    title: ' add articles'
+  });
+});
 
 // start our server
 app.listen(3000, function() {
